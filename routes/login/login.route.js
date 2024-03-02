336bf08235ac7a3c2d21036034517c3635de05ef
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-03-03 02:09:40 +0600

    error in login router resolved

--- routes/login/login.route.js
+++ routes/login/login.route.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import loginController from '../../controllers/login/login.controller.js'
-import verify from '../../middlewares/tokenVerification.cjs';
+import verify from '../../middlewares/tokenVerification.js';
 
 const router = express.Router()
 
